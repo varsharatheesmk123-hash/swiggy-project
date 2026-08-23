90cd5084391db628ed76d48481ac68d54d2f8dad
varsharatheesmk123-hash <varsharatheesmk123@gmail.com>
2026-08-23 18:52:22 +0530

    Enhance MongoDB connection logic in db.js
    Add check for existing MongoDB connection and update URI handling.

--- src/db.js
+++ src/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async() => {
+    if(mongoose.connection.readyState >= 1) return;
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URI || process.env.MONGO_URL);
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
-        process.exit(1);
+        //process.exit(1);
     }
 }
 module.exports = connectDB
